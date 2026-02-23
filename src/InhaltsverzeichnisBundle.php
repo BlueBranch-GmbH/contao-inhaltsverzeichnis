63c6b190af8e018b752ee108d465226e5e1ee880
LBeckX <lb@bluebranch.de>
2026-02-23 16:41:59 +0100

    Initial bundle scaffold

---
+++ src/InhaltsverzeichnisBundle.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Bluebranch\Inhaltsverzeichnis;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class InhaltsverzeichnisBundle extends Bundle
+{
+}
